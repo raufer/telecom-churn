77f990dfb49d4d3bdd69f5d0c1b39a5f9c4bf6c9
raufer <raufer92@deloitte.pt>
2019-07-29 10:29:39 +0100

    fixed pipeline.py

--- pipeline/analysis/analysis.py
+++ pipeline/analysis/analysis.py
@@ -29,6 +29,21 @@ def main(args):
     for i in os.listdir('artifacts'):
         print(i)
 
+    metadata = {
+        'outputs': [
+            {
+              'storage': 'inline',
+              'source': '## Customer Attrition\nCustomer attrition, also known as customer churn, customer turnover, or customer defection, is the loss of clients or customers.\nTelephone service companies, Internet service providers, pay TV companies, insurance firms, and alarm monitoring services, often use customer attrition analysis and customer attrition rates as one of their key business metrics because the cost of retaining an existing customer is far less than acquiring a new one. Companies from these sectors often have customer service branches which attempt to win back defecting clients, because recovered long-term customers can be worth much more to a company than newly recruited clients.\n```\nRows     :  7043\nColumns  :  21\nFeatures : \n ['customerID', 'gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure', 'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod', 'MonthlyCharges', 'TotalCharges', 'Churn']\n ```',
+              'type': 'markdown',
+            }
+        ]
+    }
+    with open('/mlpipeline-ui-metadata.json', 'w') as f:
+        json.dump(metadata, f)
+
+    for i in os.listdir('artifacts'):
+        print(i)
+
 
 if __name__ == "__main__":
     import os
