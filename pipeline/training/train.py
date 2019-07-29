14202bc836faefddc41f2edecda88dc852e08345
Dinis <joadinis@pt-c02yn6u7lvcg.atrema.deloitte.com>
2019-07-29 14:50:08 +0100

    Changing model to KNN.

--- pipeline/training/train.py
+++ pipeline/training/train.py
@@ -12,6 +12,8 @@ from sklearn.metrics import roc_auc_score,roc_curve,scorer
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score,recall_score
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+
 
 
 
@@ -60,10 +62,9 @@ def main(args):
     #     subsample=1
     # )
 
-    model = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-          intercept_scaling=1, max_iter=100, multi_class='ovr', n_jobs=1,
-          penalty='l2', random_state=None, solver='liblinear', tol=0.0001,
-          verbose=1, warm_start=False)
+    model = KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski',
+           metric_params=None, n_jobs=1, n_neighbors=5, p=2,
+           weights='uniform')
 
     model.fit(train_X, train_Y)
 
